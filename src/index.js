9a48837471314e44f28cb8f105eede37e35cfb38
ohige01 <ohige01@naver.com>
2024-11-05 18:15:11 +0900

    [FEAT #15] 유저 리뷰 조회 기능 구현

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";    //.env 관리 모듈
 import cors from "cors"   //cors 관리 모듈
 import express from "express";
-import { handleUserMisAdd, handleUserSignUp } from "./controllers/user.controller.js";
+import { handleListUserReviews, handleUserMisAdd, handleUserSignUp } from "./controllers/user.controller.js";
 import { handleListStoreReviews, handleReviewWrite, handleStoreAdd, handleStoreMisAdd } from "./controllers/store.controller.js";
 
 dotenv.config();    //config()를 호출해 env에 있는 내용 접근
@@ -22,6 +22,7 @@ app.get("/", (req, res) => {
 //user
 app.post("/api/v1/users/signup", handleUserSignUp);
 app.post("/api/v1/users/missions", handleUserMisAdd);
+app.get("/api/v1/users/:userId/reviews", handleListUserReviews);
 
 //store
 app.post("/api/v1/stores", handleStoreAdd);
